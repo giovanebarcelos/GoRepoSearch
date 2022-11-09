da0f63bb10aee2b9daa5129f81c0c02f930da9c5
Giovane Barcelos <giovanebarcelos@gmail.com>
2022-11-09 14:24:43 -0300

    test: should return error on SearchByText

--- test/modules/search/presenter/search/search_bloc_test.dart
+++ test/modules/search/presenter/search/search_bloc_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_repo_search/modules/search/domain/entities/entities.dart';
+import 'package:go_repo_search/modules/search/domain/errors/errors.dart';
 import 'package:go_repo_search/modules/search/domain/usecases/usecases.dart';
 import 'package:go_repo_search/modules/search/presenter/search/search_bloc.dart';
 import 'package:go_repo_search/modules/search/presenter/search/states/state.dart';
@@ -22,6 +23,23 @@ void main() {
         [
           isA<SearchLoading>(),
           isA<SearchSuccess>(),
+        ],
+      ),
+    );
+
+    bloc.add("giovane");
+  });
+
+  test('should return error', () {
+    when(() => usecase.call(any()))
+        .thenAnswer((_) async => Left(InvalidTextError()));
+
+    expectLater(
+      bloc.stream,
+      emitsInOrder(
+        [
+          isA<SearchLoading>(),
+          isA<SearchError>(),
         ],
       ),
     );
